21672183d9bbbef2c7eb7abac3da2d609bcac4ba
Tamara <tamarafrazzetta@gmail.com>
2024-08-10 12:22:52 -0300

    feat: add db connection

---
+++ config/connection.ts
@@ -0,0 +1,32 @@
+import {Db, MongoClient, ServerApiVersion} from 'mongodb'
+import dotenv from 'dotenv';
+
+dotenv.config();
+const url = process.env.MONGO_URL || '';
+
+let db: Db;
+const client = new MongoClient(url, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+export async function runDbConnection() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ping: 1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        db = client.db()
+    } finally {
+        await client.close();
+    }
+}
+
+export async function getDb() {
+    if(!db) {
+        throw new Error('Must connect to MongoDB before calling this function');
+    }
+    return db;
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import {runDbConnection} from "./config/connection";
 
-app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
-})
+const app = express()
+const port = 3000;
+
+(async () => {
+    await runDbConnection();
+    app.listen(port, () => {
+        console.log(`app listening on port ${port}`)
+    })
+})()
